238bb162b4cee4864f1c16e89360f0bd8874f966
silvasistemas <49107113+silvasistemas@users.noreply.github.com>
2020-07-13 23:13:31 -0300

    Add files via upload

---
+++ src/main/java/com/restful/webservices/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.restful.webservices.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+}
